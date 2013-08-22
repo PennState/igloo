942d665ed6c50ed4d0318d6f3e6cea379b505039
Steve Moyer <swm16@psu.edu>
2013-08-22 15:44:16 -0400

    Altered the memory-based implementation of the ProviderService to reflect the new model names.

--- igloo-memory/src/main/java/org/apache/directory/scim/memory/MemoryProvider.java
+++ igloo-memory/src/main/java/org/apache/directory/scim/memory/MemoryProvider.java
@@ -4,83 +4,78 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.directory.scim.ProviderService;
-import org.apache.directory.scim.common.Group;
-import org.apache.directory.scim.common.Response;
-import org.apache.directory.scim.common.Response.Resources;
-import org.apache.directory.scim.common.User;
+import org.apache.directory.scim.models.ScimGroup;
+import org.apache.directory.scim.models.ScimResponse;
+import org.apache.directory.scim.models.ScimUser;
 import org.apache.directory.scim.search.Query;
 
 public class MemoryProvider implements ProviderService {
 
-  public Group createGroup(Group arg0) {
+  public ScimGroup createGroup(ScimGroup arg0) {
     // TODO Auto-generated method stub
     return null;
   }
 
-  public User createUser(User arg0) {
+  public ScimUser createUser(ScimUser arg0) {
     System.out.println("Creating user: " + arg0.getUserName());
     // TODO Auto-generated method stub
     return null;
   }
 
-  public void deleteGroup(Group arg0) {
+  public void deleteGroup(ScimGroup arg0) {
     // TODO Auto-generated method stub
     
   }
 
-  public void deleteUser(User arg0) {
+  public void deleteUser(ScimUser arg0) {
     // TODO Auto-generated method stub
     
   }
 
-  public Response findGroups(Query arg0) {
-    Response response = new Response();
-    List<User> users = new ArrayList<User>();
-    Resources resources = new Resources();
-    resources.getResource().addAll(users);
-    response.setResources(resources);
+  public ScimResponse findGroups(Query arg0) {
+    ScimResponse response = new ScimResponse();
+    List<ScimGroup> groups = new ArrayList<ScimGroup>();
+    response.setResources(groups);
     // TODO Auto-generated method stub
     return response;
   }
 
-  public Response findUsers(Query arg0) {
-    Response response = new Response();
-    List<User> users = new ArrayList<User>();
-    response.setTotalResults(new Long(users.size()));
+  public ScimResponse findUsers(Query arg0) {
+    ScimResponse response = new ScimResponse();
+    List<ScimUser> users = new ArrayList<ScimUser>();
+    response.setTotalResults(users.size());
     response.setItemsPerPage(arg0.getCount());
-    Resources resources = new Resources();
-    resources.getResource().addAll(users);
-    response.setResources(resources);
+    response.setResources(users);
     // TODO Auto-generated method stub
     return response;
   }
 
-  public Group getGroup(String arg0) {
+  public ScimGroup getGroup(String arg0) {
     // TODO Auto-generated method stub
     return null;
   }
 
-  public User getUser(String arg0) {
+  public ScimUser getUser(String arg0) {
     // TODO Auto-generated method stub
     return null;
   }
 
-  public Group mergeGroup(Group arg0) {
+  public ScimGroup mergeGroup(ScimGroup arg0) {
     // TODO Auto-generated method stub
     return null;
   }
 
-  public User mergeUser(User arg0) {
+  public ScimUser mergeUser(ScimUser arg0) {
     // TODO Auto-generated method stub
     return null;
   }
 
-  public Group replaceGroup(Group arg0) {
+  public ScimGroup replaceGroup(ScimGroup arg0) {
     // TODO Auto-generated method stub
     return null;
   }
 
-  public User replaceUser(User arg0) {
+  public ScimUser replaceUser(ScimUser arg0) {
     // TODO Auto-generated method stub
     return null;
   }
