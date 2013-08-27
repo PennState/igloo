45079d489a4278c8e7e6118d88e93c168f5d2c1d
Steve Moyer <swm16@psu.edu>
2013-08-27 08:52:53 -0400

    Added the name() method to the interface (which will exist implicitly when the interface is implemented by an enum), so that the name could be generically used to generate hashcodes.

--- igloo-common/src/main/java/org/apache/directory/scim/models/ScimType.java
+++ igloo-common/src/main/java/org/apache/directory/scim/models/ScimType.java
@@ -1,5 +1,14 @@
 package org.apache.directory.scim.models;
 
 public interface ScimType {
+  
+  /**
+   * ScimTypes are generally implemented as Java enums but when we're generating
+   * hashcodes, we need access to the name of the enum element rather than the
+   * ordinal.
+   * 
+   * @return a String containing the enum value's name
+   */
+  String name();
 
 }
