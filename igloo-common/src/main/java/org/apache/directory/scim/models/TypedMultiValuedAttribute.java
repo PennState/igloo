f5b1ae08431392fddf8dfaaf780e34b6501f6cec
Steve Moyer <swm16@psu.edu>
2013-08-27 08:54:18 -0400

    Used the name() method of the "type" to generate the hashcode since the name will be consistent across application restarts.

--- igloo-common/src/main/java/org/apache/directory/scim/models/TypedMultiValuedAttribute.java
+++ igloo-common/src/main/java/org/apache/directory/scim/models/TypedMultiValuedAttribute.java
@@ -26,7 +26,10 @@ public abstract class TypedMultiValuedAttribute<T extends ScimType> extends Mult
   public int hashCode() {
     final int prime = 31;
     int result = super.hashCode();
-    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    // ***** WARNING *****
+    // The type enum's name is used here so that the resulting hashcode will
+    // be consistent across JVM restarts.
+    result = prime * result + ((type == null) ? 0 : type.name().hashCode());
     return result;
   }
 
