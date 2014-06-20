fcde7d7a2d17ad50311948bc41aae4bad2f19d3c
ses44 <ses44@psu.edu>
2014-06-20 14:54:08 -0400

    Changing the resources so that we not explicitly consume and produce only json

--- igloo-server/src/main/java/org/apache/directory/scim/UserResource.java
+++ igloo-server/src/main/java/org/apache/directory/scim/UserResource.java
@@ -58,7 +58,8 @@ import org.slf4j.LoggerFactory;
  *         Project</a>
  */
 @Path("Users")
-@Produces( {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML} )
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(UserResource.class);
